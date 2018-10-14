9f2dbe3ecadd05e5188e564a59888fd3a67a3784
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-10-14 16:10:37 -0400

    Added routes folder

--- nodejs-service/index.js
+++ nodejs-service/index.js
@@ -1,28 +1,32 @@
-// Copyright 2018, Google LLC.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
-'use strict';
+var app = express();
 
-const express = require('express');
+var main = require('./routes/main');
+app.use('/', main);
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// [START hello_world]
-// Say hello!
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-// [END hello_world]
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 if (module === require.main) {
   // [START server]
