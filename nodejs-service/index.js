7f2c1d386dd7cd42abab8904dd777ea25ed76392
DinoMos <mufticdino@gmail.com>
2018-10-15 20:05:23 -0400

    login and dashboard page

--- nodejs-service/index.js
+++ nodejs-service/index.js
@@ -40,3 +40,31 @@ if (module === require.main) {
 }
 
 module.exports = app;
+
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  var path = '.' + req.url;
+
+  // Default to index.html if no file is specified.
+  if (req.url.split('.').length === 1) {
+    path += (req.url[req.url.length - 1] !== '/' ? '/' : '') + 'login.html';
+  }
+
+  // Check if resource exists and serve 404 page if it doesn't.
+  fs.exists(path, function (exists) {
+    if ( ! exists) {
+      path = './Pages/404.html';
+    }
+ 
+    fs.readFile(path, function (err, html) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(html + '');  
+      res.end();
+    });
+   
+    
+  });
+})
+.listen(3000, '127.0.0.1');
